0f9f9d3151bb3c22cce6496a86fa6752b1fb4433
Pedrohdsg <pedrohdsg@gmail.com>
2023-11-10 16:45:07 -0300

    Concluindo API

--- api/routes/pessoasRoute.js
+++ api/routes/pessoasRoute.js
@@ -7,5 +7,18 @@ router.get("/pessoas/:id", PessoaController.pegaUmaPessoa);
 router.post("/pessoas", PessoaController.criarPessoa);
 router.put("/pessoas/:id", PessoaController.atualizaPessoa);
 router.delete("/pessoas/:id", PessoaController.deletaPessoa);
+router.get(
+  "/pessoas/:estudanteId/matricula/:matriculaId",
+  PessoaController.pegaUmaMatricula
+);
+router.post("/pessoas/:estudanteId/matricula", PessoaController.criarMatricula);
+router.put(
+  "/pessoas/:estudanteId/matricula/:matriculaId",
+  PessoaController.atualizaMatricula
+);
+outer.delete(
+    "/pessoas/:estudanteId/matricula/:matriculaId",
+    PessoaController.deletaMatricula
+  );
 
 module.exports = router;
